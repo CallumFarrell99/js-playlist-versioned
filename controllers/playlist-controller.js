322aa52a48da97f8608b06949386867c4df2aeb8
Callum Farrell <20104331@mail.wit.ie>
2023-08-01 22:50:16 +0100

    Added functionality to add and list tracks to a playlist

--- controllers/playlist-controller.js
+++ controllers/playlist-controller.js
@@ -1,4 +1,5 @@
 import { playlistStore } from "../models/playlist-store.js";
+import { trackStore } from "../models/track-store.js";
 
 export const playlistController = {
     async index(request, response) {
@@ -9,5 +10,17 @@ export const playlistController = {
         };
         console.log(`rendering playlist: ${playlist.title}`)
         response.render("playlist-view", viewData);
+    },
+
+    async addTrack(request, response) {
+        const playlist = await playlistStore.getPlaylistById(request.params.id);
+        const newTrack = {
+            title: request.body.title,
+            artist: request.body.artist,
+            duration: Number(request.body.duration),
+        };
+        console.log(`adding track ${newTrack.title}`);
+        await trackStore.addTrack(playlist._id, newTrack);
+        response.redirect("/playlist/" + playlist._id);
     },
 };
