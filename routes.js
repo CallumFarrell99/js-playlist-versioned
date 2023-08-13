448d667e23a971fa3b54117f1257138bcdbbefe9
Callum Farrell <20104331@mail.wit.ie>
2023-08-13 17:34:25 +0100

    Added routes for accounts and logout button to menu

--- routes.js
+++ routes.js
@@ -3,9 +3,17 @@ import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { playlistController } from "./controllers/playlist-controller.js";
 
+const accounts = require('./controllers/accounts.js');
+
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
 router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
 router.get("/about", aboutController.index);
@@ -13,3 +21,5 @@ router.get("/playlist/:id", playlistController.index);
 router.post("/playlist/:id/addtrack", playlistController.addTrack);
 router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack);
+
+
